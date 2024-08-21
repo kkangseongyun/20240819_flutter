dcd32b97f0a2114fc491bd8e113d889d698b606d
kkang <kkang104@gmail.com>
2024-08-21 13:40:34 +0900

    Add files via upload

--- ch4/stack.dart
+++ ch4/stack.dart
@@ -10,7 +10,39 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Layout Test'),
         ),
-        
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.pink,
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.0),//center
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('images/lab_lotte_1.jpg', width: 350,),
+                      Container(
+                        width: 350,
+                        height: 100,
+                        color: Colors.white,
+                        padding: EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
